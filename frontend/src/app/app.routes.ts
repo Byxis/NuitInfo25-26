02026e001cebfbdfe20a70f1c861a7b5fcc4f7b1
Sebastien <askneuh@gmail.com>
2025-12-05 04:13:13 +0100

    finalization of quiz

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { AdminComponent } from '@admin/admin/admin.component';
 import { authGuard } from '@auth/auth.guard';
 import { adminGuard } from '@admin/admin.guard';
 import { Quizz } from './pages/quizz/quizz';
+import { QuizComponent } from './pages/quiz-component/quiz-component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginPage },
   { path: 'home', component: HomePage, canActivate: [authGuard] },
   { path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] },
   { path: 'quizz', component: Quizz, canActivate: [authGuard]},
+  { path: 'quizComponent', component: QuizComponent, canActivate: [authGuard]},
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', redirectTo: 'home' },
 ];
--- frontend/src/app/pages/quizz/quizz.ts
+++ frontend/src/app/pages/quizz/quizz.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal} from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ComponentKey } from '../../shared/types/nird-composant.enum';
 
 // Interface pour définir la structure de chaque explication de composant
 interface ComposantNIRD {
@@ -11,109 +13,105 @@ interface ComposantNIRD {
 
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './quizz.html',
   styleUrl: './quizz.scss'
 })
 export class Quizz {
-
-    boxVisible = false
-    GPUVisible = false
-    CPUVisible = false
-    MBVisible = false
-    alimVisible = false
-    diskVisible = false
-    RAMVisible = false
+    readonly router = inject(Router);
+    selectedComponent = signal<ComponentKey>(ComponentKey.NONE);
+    readonly ComponentKey = ComponentKey;
+    
+    selectComponent(key: ComponentKey) {
+    // Si la clé cliquée est déjà la clé sélectionnée, on désélectionne (NONE)
+    // Sinon, on met à jour le signal avec la nouvelle clé
+    this.selectedComponent.update(currentKey => 
+      currentKey === key ? ComponentKey.NONE : key
+    );
+  }
 
     box: ComposantNIRD = 
-    {
-      nom: 'Le Boîtier (La Tour)',
-      enjeuNird: 'Durabilité et Reconditionnement',
-      explication:
-        "C'est l'enveloppe du PC. Il symbolise la durabilité physique. La démarche NIRD encourage le reconditionnement et la réutilisation des équipements pour lutter contre l'obsolescence programmée. Un boîtier réutilisable permet de mettre à jour uniquement les pièces internes.",
-      questionQuizz: 'Quel terme désigne l\'action de remettre un équipement en état pour lui donner une seconde vie ?',
-      reponseQuizz: 'Reconditionnement',
-    }
-    alim: ComposantNIRD =
-    {
-      nom: 'L\'Alimentation (PSU)',
-      enjeuNird: 'Sobriété et Responsabilité Énergétique',
-      explication:
-        "Ce bloc gère la consommation énergétique de tout le PC. Choisir un bloc de bonne qualité et correctement dimensionné contribue à la sobriété en limitant les pertes. Le numérique responsable commence par la maîtrise de l'énergie.",
-      questionQuizz: 'Outre la performance, quel critère essentiel permet de réduire l\'impact énergétique d\'un composant ?',
-      reponseQuizz: 'La Sobriété / L\'Efficacité énergétique',
-    }
-
-
-    CPU: ComposantNIRD =
-    {
-      nom: 'Le Processeur (CPU)',
-      enjeuNird: 'Sobriété et Impact Carbone de la Production',
-      explication:
-        "C'est le cerveau du PC. Il est très complexe à fabriquer et sa production a un impact carbone important. La sobriété implique de choisir un processeur adapté aux besoins réels (ne pas surdimensionner) et de le conserver le plus longtemps possible.",
-      questionQuizz: 'Quel facteur (souvent oublié) des équipements informatiques est le plus impactant pour l\'environnement : la fabrication ou l\'utilisation ?',
-      reponseQuizz: 'La Fabrication (ou l\'extraction des matières premières)',
-    }
-
-    GPU: ComposantNIRD = 
-    {
-      nom: 'La Carte Graphique (GPU)',
-      enjeuNird: 'Frugalité et Choix Technologique',
-      explication:
-        "Souvent très puissante et coûteuse en énergie à l'usage, la carte graphique doit être utilisée avec frugalité. Pour les usages éducatifs ou bureautiques, on peut privilégier une solution graphique intégrée au processeur (IGP) pour économiser des ressources et de l'énergie.",
-      questionQuizz: 'Pour un usage bureautique, quel type de solution graphique favorise la frugalité ?',
-      reponseQuizz: 'Une solution graphique intégrée (IGP)',
-    }
-
-    RAM: ComposantNIRD = 
-    {
-      nom: 'La Mémoire Vive (RAM)',
-      enjeuNird: 'Inclusivité et Accès Équitable',
-      explication:
-        "La RAM affecte la vitesse du PC. Des logiciels et OS frugaux (peu gourmands) permettent de réduire les besoins en RAM. Cela rend les machines plus anciennes utilisables, favorisant l'inclusivité et l'accès équitable à des outils performants pour tous.",
-      questionQuizz: 'Utiliser des logiciels "frugaux" permet de réutiliser de vieilles machines. Quel pilier NIRD est directement lié à l\'accès équitable aux outils ?',
-      reponseQuizz: 'L\'Inclusivité',
-    }
-
-    MotherBoard: ComposantNIRD =
-    {
-      nom: 'La Carte Mère',
-      enjeuNird: 'Souveraineté Logicielle (OS)',
-      explication:
-        "C'est le socle du système. Elle détermine la compatibilité avec les systèmes d'exploitation (OS). NIRD promeut l'usage des logiciels libres (comme Linux) pour s'affranchir de la dépendance aux éditeurs propriétaires et garantir une meilleure souveraineté numérique sur les outils éducatifs.",
-      questionQuizz: 'Quel type de logiciel est favorisé par NIRD pour garantir une meilleure autonomie et souveraineté numérique ?',
-      reponseQuizz: 'Logiciel Libre / Open Source',
-    }
-
-    Disk: ComposantNIRD = 
-    {
-      nom: 'Le Disque Dur / SSD',
-      enjeuNird: 'Responsabilité et Sécurité des Données',
-      explication:
-        "Ce composant stocke les données localement. L'enjeu est de choisir où stocker ses données (localement, nationalement, ou sur des plateformes externes). NIRD recommande la maîtrise de l'hébergement pour garantir la sécurité et la souveraineté des données éducatives.",
-      questionQuizz: 'Maîtriser où sont stockées et traitées les données (via un hébergement local ou national) contribue principalement à quel enjeu ?',
-      reponseQuizz: 'Souveraineté / Sécurité des Données',
-    }
-
-  toggleDisplayAlim() {
-    this.alimVisible = !this.alimVisible;
+  {
+    nom: 'Le Boîtier (La Tour)',
+    enjeuNird: 'Durabilité et Reconditionnement',
+    explication:
+      "C'est l'enveloppe du PC. Il symbolise la durabilité physique. La démarche NIRD encourage le reconditionnement et la réutilisation des équipements pour lutter contre l'obsolescence programmée. Un boîtier réutilisable permet de mettre à jour uniquement les pièces internes.",
+    // Nouvelle Q/R pour le Boîtier
+    questionQuizz: 'Pourquoi réutiliser un boîtier plutôt que d’en acheter un neuf contribue au numérique responsable ?',
+    reponseQuizz: 'Parce que réutiliser évite de fabriquer du matériel inutilement et allonge la durée de vie du PC.',
   }
-  toggleDisplayBox() {
-    this.boxVisible = !this.boxVisible
+  
+  alim: ComposantNIRD =
+  {
+    nom: 'L\'Alimentation (PSU)',
+    enjeuNird: 'Sobriété et Responsabilité Énergétique',
+    explication:
+      "Ce bloc gère la consommation énergétique de tout le PC. Choisir un bloc de bonne qualité et correctement dimensionné contribue à la sobriété en limitant les pertes. Le numérique responsable commence par la maîtrise de l'énergie.",
+    // Nouvelle Q/R pour l'Alimentation
+    questionQuizz: 'Pourquoi est-il important de choisir une alimentation adaptée à la puissance du PC ?',
+    reponseQuizz: 'Pour éviter de gaspiller de l’énergie et limiter les pertes électriques.',
   }
-  toggleDisplayCPU() {
-    this.CPUVisible = !this.CPUVisible
+
+
+  CPU: ComposantNIRD =
+  {
+    nom: 'Le Processeur (CPU)',
+    enjeuNird: 'Sobriété et Impact Carbone de la Production',
+    explication:
+      "C'est le cerveau du PC. Il est très complexe à fabriquer et sa production a un impact carbone important. La sobriété implique de choisir un processeur adapté aux besoins réels (ne pas surdimensionner) et de le conserver le plus longtemps possible.",
+    // Nouvelle Q/R pour le CPU
+    questionQuizz: 'Pourquoi vaut-il mieux choisir un processeur suffisant plutôt que très puissant si l’on n’en a pas besoin ?',
+    reponseQuizz: 'Parce que fabriquer un processeur puissant pollue davantage, et un modèle trop puissant consomme plus pour rien.',
   }
-  toggleDisplayRAM() {
-    this.RAMVisible = !this.RAMVisible
+
+  GPU: ComposantNIRD = 
+  {
+    nom: 'La Carte Graphique (GPU)',
+    enjeuNird: 'Frugalité et Choix Technologique',
+    explication:
+      "Souvent très puissante et coûteuse en énergie à l'usage, la carte graphique doit être utilisée avec frugalité. Pour les usages éducatifs ou bureautiques, on peut privilégier une solution graphique intégrée au processeur (IGP) pour économiser des ressources et de l'énergie.",
+    // Nouvelle Q/R pour le GPU
+    questionQuizz: 'Dans quels cas une carte graphique intégrée est un bon choix ?',
+    reponseQuizz: 'Pour la bureautique ou les usages simples, car elle consomme moins d’énergie et suffit largement.',
   }
-  toggleDisplayDisk() {
-    this.diskVisible = !this.diskVisible
+
+  RAM: ComposantNIRD = 
+  {
+    nom: 'La Mémoire Vive (RAM)',
+    enjeuNird: 'Inclusivité et Accès Équitable',
+    explication:
+      "La RAM affecte la vitesse du PC. Des logiciels et OS frugaux (peu gourmands) permettent de réduire les besoins en RAM. Cela rend les machines plus anciennes utilisables, favorisant l'inclusivité et l'accès équitable à des outils performants pour tous.",
+    // Nouvelle Q/R pour la RAM
+    questionQuizz: 'Comment des logiciels “légers” peuvent-ils aider les vieux ordinateurs à rester utiles ?',
+    reponseQuizz: 'Ils demandent moins de RAM, ce qui permet aux anciens PC de fonctionner correctement.',
   }
-  toggleDisplayMB() {
-    this.MBVisible = !this.MBVisible
+
+  MotherBoard: ComposantNIRD =
+  {
+    nom: 'La Carte Mère',
+    enjeuNird: 'Souveraineté Logicielle (OS)',
+    explication:
+      "C'est le socle du système. Elle détermine la compatibilité avec les systèmes d'exploitation (OS). NIRD promeut l'usage des logiciels libres (comme Linux) pour s'affranchir de la dépendance aux éditeurs propriétaires et garantir une meilleure souveraineté numérique sur les outils éducatifs.",
+    // Nouvelle Q/R pour la Carte Mère
+    questionQuizz: 'Pourquoi utiliser un système d’exploitation libre renforce-t-il la souveraineté numérique ?',
+    reponseQuizz: 'Parce qu’on dépend moins d’entreprises privées et qu’on peut contrôler ce que fait le logiciel.',
   }
-  toggleDisplayGPU() {
-    this.GPUVisible = !this.GPUVisible
+
+  Disk: ComposantNIRD = 
+  {
+    nom: 'Le Disque Dur / SSD',
+    enjeuNird: 'Responsabilité et Sécurité des Données',
+    explication:
+      "Ce composant stocke les données localement. L'enjeu est de choisir où stocker ses données (localement, nationalement, ou sur des plateformes externes). NIRD recommande la maîtrise de l'hébergement pour garantir la sécurité et la souveraineté des données éducatives.",
+    // Nouvelle Q/R pour le Disque
+    questionQuizz: 'Pourquoi savoir où sont stockées nos données est-il important ?',
+    reponseQuizz: 'Pour protéger les informations, éviter qu’elles partent à l’étranger et garantir leur sécurité.',
+  }
+  goToQuiz() {
+    this.router.navigate(['quizComponent']);
   }
 }
