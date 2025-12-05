1ecbfcb27bb1b2882fddd79e0987b32c83b2bbd8
Alessandra <alessandra.van-rossen-martinez@etu.umontpellier.fr>
2025-12-05 03:19:41 +0100

    feat: merged the games

--- frontend/src/app/elements/desktop/desktop.ts
+++ frontend/src/app/elements/desktop/desktop.ts
@@ -6,6 +6,10 @@ import { FolderWindowComponent } from '../folder-window/folder-window';
 import { AppIconComponent } from '../app-icon/app-icon';
 import { Accueil } from 'src/app/pages/accueil/accueil';
 import { Window } from '../window/window';
+import { SnakeComponent } from 'src/app/pages/snake/snake';
+import { TowerDefenseComponent } from 'src/app/pages/tower-defense/tower-defense.component';
+import { TaskManager } from 'src/app/pages/task-manager/task-manager';
+import { LaserBeamComponent } from 'src/app/pages/laser-game/laser-beam/laser-beam';
 
 @Component({
   selector: 'app-desktop',
@@ -15,7 +19,10 @@ import { Window } from '../window/window';
 })
 export class DesktopComponent {
   apps: App[] = [
-    {id: 'accueil', name: 'Accueil', icon: 'file.png', type: 'txt', component: Accueil},
+    {id: 'accueil', name: 'Accueil.txt', icon: 'file.png', type: 'txt', component: Accueil},
+    {id: 'snake', name: 'Snake.exe', icon: 'file.png', type: 'txt', component: SnakeComponent},
+    {id: 'tower', name: 'Tower Defense.exe', icon: 'file.png', type: 'txt', component: TowerDefenseComponent},
+    {id: 'task', name: 'Task manager.exe', icon: 'file.png', type: 'txt', component: TaskManager},
   ]
     //{id: 'jeux', name: 'Jeux', icon: '', type: 'folder', component: FolderWindowComponent}
 
