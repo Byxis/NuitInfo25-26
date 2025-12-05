965dd2d262aeff60d7c181e0d0e230524892a397
alexis.serrano <alexis.serrano@etu.umontpellier.fr>
2025-12-05 03:58:18 +0100

    Feat: add credits and team pages; Refactor: update accueil content
    Introduces new 'Crédits' and 'Équipe' pages with HTML, SCSS, and TypeScript files, displaying resource links and team member info. Updates the desktop app list to include these pages. Enhances the 'Accueil' page with detailed event information and styles.

--- frontend/src/app/elements/desktop/desktop.ts
+++ frontend/src/app/elements/desktop/desktop.ts
@@ -6,31 +6,33 @@ import { FolderWindowComponent } from '../folder-window/folder-window';
 import { AppIconComponent } from '../app-icon/app-icon';
 import { Accueil } from 'src/app/pages/accueil/accueil';
 import { Window } from '../window/window';
+import { Equipe } from 'src/app/pages/equipe/equipe';
+import { Credits } from 'src/app/pages/credits/credits';
 
 @Component({
   selector: 'app-desktop',
   imports: [TaskbarComponent, AppIconComponent, Window],
   templateUrl: './desktop.html',
-  styleUrl: './desktop.scss'
+  styleUrl: './desktop.scss',
 })
 export class DesktopComponent {
   apps: App[] = [
-    {id: 'accueil', name: 'Accueil', icon: 'file.png', type: 'txt', component: Accueil},
-  ]
-    //{id: 'jeux', name: 'Jeux', icon: '', type: 'folder', component: FolderWindowComponent}
+    { id: 'accueil', name: 'Accueil', icon: 'file.png', type: 'txt', component: Accueil },
+    { id: 'credits', name: 'Crédits', icon: 'file.png', type: 'txt', component: Credits },
+    { id: 'equipe', name: "L'Équipe", icon: 'file.png', type: 'txt', component: Equipe },
+  ];
+  //{id: 'jeux', name: 'Jeux', icon: '', type: 'folder', component: FolderWindowComponent}
 
   openWindows: any[] = [];
 
   openApp(app: App) {
-    const alreadyOpen = this.openWindows.some(win => win.id === app.id);
+    const alreadyOpen = this.openWindows.some((win) => win.id === app.id);
 
-    if(alreadyOpen)
-      return
-    this.openWindows.push({id: app.id, name: app.name, component: app.component});
-
+    if (alreadyOpen) return;
+    this.openWindows.push({ id: app.id, name: app.name, component: app.component });
   }
 
   closeApp(win: any) {
-    this.openWindows = this.openWindows.filter(w => w.id !== win.id);
+    this.openWindows = this.openWindows.filter((w) => w.id !== win.id);
   }
 }
